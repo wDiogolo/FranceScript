9b86c60398f04d31bc682e35b946a65526351b41
Seyz123 <seylio@outlook.com>
2019-12-14 15:41:57 +0100

    Exécute directement le code.

--- src/cli.js
+++ src/cli.js
@@ -17,5 +17,5 @@ fs.readFile(filepath, 'utf8', function (err, content) {
         return process.exit(1);
     }
 
-    eval(core.compile(content, "fr")));
+    eval(core.compile(content, "fr"));
 });
