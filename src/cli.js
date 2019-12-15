4644282b3bf1eaa64e7a17789265660c236916a2
Seyz123 <seylio@outlook.com>
2019-12-15 15:42:55 +0100

    .

--- src/cli.js
+++ src/cli.js
@@ -29,6 +29,7 @@ const version = require("../package.json");
 				
 				if(file === "package.json"){
 					let json = JSON.parse(readFileSync(filePath).toString());
+					console.log(json);
 					let main = json.main.replace("./", "").split("/")[0];
 					
 					let ls = lstatSync(main);
